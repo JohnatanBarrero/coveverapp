57d3ef80498b25571cec8262122bf5a3e0bdd768
JohnatanBarrero <jonathan.barrero@hotmail.com>
2024-03-16 15:06:31 -0600

    addiNTL

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -56,7 +56,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
 
   List<Locale> get supportedLocales {
     return const <Locale>[
-      Locale.fromSubtags(languageCode: 'es'),
+      Locale.fromSubtags(languageCode: 'en'),
     ];
   }
 
